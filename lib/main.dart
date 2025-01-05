daa59370c3c152ed90f47e67ff26a9dcc690d845
João Gabriel Ramos <jgbvramos@id.uff.br>
2025-01-05 13:03:54 -0300

    feat: referência à busca.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:sudoku/pages/busca.dart';
 import 'package:sudoku/pages/game.dart';
 import 'package:sudoku/pages/home.dart';
 
@@ -9,7 +10,8 @@ void main() {
     initialRoute: "/",
     routes: {
       "/home": (context) => HomePage(),
-      Game.routeName: (context) => Game()
+      Game.routeName: (context) => Game(),
+      "/busca": (context) => Busca(),
     },
     title: "Sudoku",
     debugShowCheckedModeBanner: false,
